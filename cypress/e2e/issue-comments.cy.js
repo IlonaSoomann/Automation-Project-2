ea31fa313c41046ac1a926c3cf81d4cd5fd18577
ilonasoomann <ilonasoomann@gmail.com>
2023-07-27 13:07:40 +0300

    My commets about the commit

--- cypress/e2e/issue-comments.cy.js
+++ cypress/e2e/issue-comments.cy.js
@@ -68,4 +68,61 @@ describe('Issue comments creating, editing and deleting', () => {
             .find('[data-testid="issue-comment"]')
             .should('not.exist');
     });
+
+    it('Should add, edit and delete a comment successfully', () => {
+        const comment = 'TEST_COMMENT';
+        const previousComment = 'An old silent pond...';
+        const comment2 = 'EDITED_COMMENT';
+
+        getIssueDetailsModal().within(() => {
+            cy.contains('Add a comment...')
+                .click();
+
+            cy.get('textarea[placeholder="Add a comment..."]').type(comment);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.contains('Add a comment...').should('exist');
+            cy.get('[data-testid="issue-comment"]').should('contain', comment);
+
+            cy.get('[data-testid="issue-comment"]')
+                .eq('1')
+                .should('contain', previousComment)
+                .contains('Edit')
+                .click()
+                .should('not.exist');
+
+            cy.get('textarea[placeholder="Add a comment..."]')
+                .should('contain', previousComment)
+                .clear()
+                .type(comment2);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.get('[data-testid="issue-comment"]')
+                .should('contain', 'Edit')
+                .and('contain', comment2);
+
+            cy.get('[data-testid="issue-comment"]')
+                .should('have.length', 2) 
+                .and('contain', comment)   
+                .first()
+                .contains('Delete')
+                .click();
+        });
+
+        cy.get('[data-testid="modal:confirm"]')
+            .contains('button', 'Delete comment')
+            .click()
+            .should('not.exist');
+
+        getIssueDetailsModal()
+            cy.get('[data-testid="issue-comment"]')
+            .contains(comment)
+            .should('not.exist');
+    });
 });
